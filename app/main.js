30ed39da1317bb321d887e3fc6cffbe734ac32c6
Morgante Pell <morgante.pell@morgante.net>
2015-05-15 01:36:10 -0400

    rename and ye shall understand

--- app/main.js
+++ app/main.js
@@ -9,9 +9,9 @@
 
 var React = require('react');
 
-var TodoApp = require('./components/TodoApp.react');
+var TodoApp = require('./components/todo_app');
 
 React.render(
-  <TodoApp />,
-  document.getElementById('todoapp')
+	<TodoApp />,
+	document.getElementById('todoapp')
 );
