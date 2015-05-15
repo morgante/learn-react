0d8b4545847b0dd19691eaa24849fd6e5645edfc
Morgante Pell <morgante.pell@morgante.net>
2015-05-15 00:16:19 -0400

    whoah saving and syncing to server with comment box, I love it

--- app/commentform.js
+++ app/commentform.js
@@ -1,13 +1,26 @@
 var React = require('react');
 
 var CommentForm = React.createClass({
-  render: function() {
-    return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
-    );
-  }
+	handleSubmit: function(e) {
+		e.preventDefault();
+		var author = React.findDOMNode(this.refs.author).value.trim();
+		var text = React.findDOMNode(this.refs.text).value.trim();
+		if (!text || !author) {
+			return;
+		}
+		this.props.onCommentSubmit({author: author, text: text});
+		React.findDOMNode(this.refs.author).value = '';
+		React.findDOMNode(this.refs.text).value = '';
+		return;
+	},
+	render: function() {
+		return (
+			<form className="commentForm" onSubmit={this.handleSubmit}>
+				<input type="text" placeholder="Your name" ref="author" />
+				<input type="text" placeholder="Say something..." ref="text" />
+				<input type="submit" value="Post" />
+			</form>
+		);
+	}
 });
-
 module.exports = CommentForm;
