8fb1d6efe34608db10cc94a09085a825062b0363
Morgante Pell <morgante.pell@morgante.net>
2015-05-14 23:49:33 -0400

    whoah pass data for comment box

--- app/main.js
+++ app/main.js
@@ -2,8 +2,13 @@ var React = require('react');
 
 var CommentBox = require('./commentbox');
 
+var data = [
+  {author: "Peta!", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 React.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );
 
