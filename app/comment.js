00cf06fa94cdb505f48dde6c742ba4800e0c48bd
Morgante Pell <morgante.pell@morgante.net>
2015-05-14 23:45:46 -0400

    whoah markdown whoah

--- app/comment.js
+++ app/comment.js
@@ -1,13 +1,15 @@
 var React = require('react');
+var marked = require('marked');
 
 module.exports = React.createClass({
   render: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
     return (
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
