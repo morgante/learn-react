63905d15518cfbc1faa8a7c3454db2241731f2b4
Morgante Pell <morgante.pell@morgante.net>
2015-05-15 00:38:53 -0400

    nix that comments stuff; it's todo time

--- server/index.js
+++ server/index.js
@@ -10,25 +10,6 @@ function start() {
 
 	app.use(express.static(__dirname + '/../public'));
 
-	app.get('/comments.json', function(req, res) {
-		fs.readFile(__dirname + '/comments.json', function(err, data) {
-			res.setHeader('Content-Type', 'application/json');
-			res.send(data);
-		});
-	});
-
-	app.post('/comments.json', function(req, res) {
-		fs.readFile(__dirname + '/comments.json', function(err, data) {
-			var comments = JSON.parse(data);
-			comments.push(req.body);
-			fs.writeFile(__dirname + '/comments.json', JSON.stringify(comments, null, 4), function(err) {
-				res.setHeader('Content-Type', 'application/json');
-				res.setHeader('Cache-Control', 'no-cache');
-				res.send(JSON.stringify(comments));
-			});
-		});
-	});
-
 	app.listen(3000);
 
 	console.log("listening");
